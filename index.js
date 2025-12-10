618789444160f6216375760f4aa71d26dc5e12f2
Nure Anha Tamanna <nureanha99@gmail.com>
2025-12-11 02:48:04 +0600

    Vercel add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require ("dotenv").config() ;
 const express = require ("express") ;  // hook type of mongoDB 
 const cors = require ("cors") ;   // Cors - permitted to run in localhost:5173-->frontend and localhost:3000 -->backend
-require ("dotenv").config() ;
 const port = process.env.PORT || 3000 ;
 
 
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
 
@@ -166,7 +166,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
